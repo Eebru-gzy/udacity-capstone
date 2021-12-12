e03b6347c9a78b51f79df0373b7baf1d25efaef1
Opeyemi Alao <alaoopeyemi101@gmail.com>
2021-12-12 21:45:07 +0100

    add v1 working evidence

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(morgan("tiny"));
 
 //The very root route
 app.get("/", (req, res) => {
-	res.send({ message: "WELCOME!! Backend For Alles Insurance " });
+	res.send({ message: "WELCOME!! This is App Version1" });
 });
 
 app.get("/healthz", (req, res) => {
